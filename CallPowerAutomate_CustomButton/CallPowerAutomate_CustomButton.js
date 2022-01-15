2ba891a4f36fe2d13ca88ce27f7c01812c3d9a09
Mar <47777338+mpedroche@users.noreply.github.com>
2022-01-15 20:16:41 +0100

    Update CallPowerAutomate_CustomButton.js

--- CallPowerAutomate_CustomButton/CallPowerAutomate_CustomButton.js
+++ CallPowerAutomate_CustomButton/CallPowerAutomate_CustomButton.js
@@ -1,11 +1,13 @@
-function CallPowerAutomate(executionContext) {
-    var formContext = executionContext.getFormContext();
-    var recordId = executionContext.entityReference.id.replace('{', '').replace('}', '');
+function CallPowerAutomate(primaryControl) {
+    var formContext = primaryControl;
+    var recordId = formContext.data.entity.getId().replace('{', '').replace('}', '');
     var data = {
         "recordId": recordId
     };
+    var alertStrings = { text:"Ocurrio un error, intentelo de nuevo. Si el problema persiste contacte con el administrador", title:"ERROR", confirmButtonLabel:"Aceptar"};
+	var alertOptions = { height: 200, width: 450  };
     var req = new XMLHttpRequest();
-    req.open("POST", "PowerAutomateURLHTTP_Trigger", true);
+    req.open("POST", "https://prod-193.westeurope.logic.azure.com:443/workflows/046fe07b1b694752bb57bcbb83ce7b1a/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=C0LJYynFIbAIT2SElzRHUWOMsSI-mEfW0YdLqIBrtz4", true);
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     req.onreadystatechange = function () {
@@ -14,17 +16,19 @@ function CallPowerAutomate(executionContext) {
 
             if (this.status === 200) {
                 console.log("Flow Call finished successfully");
+                Xrm.Utility.closeProgressIndicator();
                 var entityFormOptions = {};
                 entityFormOptions["entityName"] = formContext.data.entity.getEntityName();
                 entityFormOptions["entityId"] = formContext.data.entity.getId();
                 Xrm.Navigation.openForm(entityFormOptions);
             } else {
+                Xrm.Utility.closeProgressIndicator();
                 console.log("Flow Call finished with error. Status code: " + this.status);
-                alert('Ocurrió un error durante el proceso, inténtelo más tarde.');
+                Xrm.Navigation.openAlertDialog(alertStrings,alertOptions).then(closeCallback,errorCallback);
             }
         }
     };
     req.send(JSON.stringify(data));
+    Xrm.Utility.showProgressIndicator("Please wait...");
     console.log("Flow Call request sent");
-    alert('Proceso ejecutado correctamente. La página se recargará cuando termine el proceso.');
 };
