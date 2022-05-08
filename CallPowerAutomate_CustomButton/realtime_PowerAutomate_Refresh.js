871d903eaf80c657e4879f71adc2dfe1a24507e5
Mar <47777338+mpedroche@users.noreply.github.com>
2022-05-08 19:58:34 +0200

    Update realtime_PowerAutomate_Refresh.js

--- CallPowerAutomate_CustomButton/realtime_PowerAutomate_Refresh.js
+++ CallPowerAutomate_CustomButton/realtime_PowerAutomate_Refresh.js
@@ -1,4 +1,5 @@
-function CallPowerAutomate(primaryControl) {
+
+async function CallPowerAutomate(primaryControl) {
 
     //Obtenemos el contexto de ejecución.
     var formContext = primaryControl;
@@ -9,51 +10,33 @@ function CallPowerAutomate(primaryControl) {
         "recordId": recordId
     };
     
+    //Generamos la variables del cuadro de dialogo que se abrirá en caso correcto.
+    var alertStrings = { text: "El proceso finalizó correctamente", title: "CORRECTO", confirmButtonLabel: "Ok" };
+    var alertOptions = { height: 200, width: 450 };
     //Generamos la variables del cuadro de dialogo que se abrirá en caso de error.
-    var alertStrings = { text:"Ocurrió un error, intentelo de nuevo. Si el problema persiste contacte con el administrador", title:"ERROR", confirmButtonLabel:"Aceptar"};
-    var alertOptions = { height: 200, width: 450  };
-    
-    //Haremos la llamada a nuestro Power Automate.
-    var req = new XMLHttpRequest();
-    req.open("POST", "PowerAutomateURL", true);
-    req.setRequestHeader("Accept", "application/json");
-    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    req.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            req.onreadystatechange = null;
-
-            if (this.status === 200) {
-            
-                //Si se ejecuta correctamente.
-                console.log("Flow Call finished successfully");
-                
-                //Cerramos el indicador de proceso.
-                formContext.data.refresh(false).then(
-                    function success(results){
-                        Xrm.Utility.closeProgressIndicator();
-                    }
-                    , function (error) {
-                        console.log(error.message);
-                    }
-                    );    
-                //Refrescamos el registro, para actualizar los cambios en los datos.
-                //var entityFormOptions = {};
-                //entityFormOptions["entityName"] = formContext.data.entity.getEntityName();
-                //entityFormOptions["entityId"] = formContext.data.entity.getId();
-                //Xrm.Navigation.openForm(entityFormOptions);
-            } else {
-            
-                //Si NO se ejecuta correctamente.
-                Xrm.Utility.closeProgressIndicator();
-                console.log("Flow Call finished with error. Status code: ")
-                //Abrimos cuadro de dialogo con alerta.
-                Xrm.Navigation.openAlertDialog(alertStrings,alertOptions).then(closeCallback,errorCallback);
-            }
-        }
-    };
-    req.send(JSON.stringify(data));
-    
-    //Iniciamos un indicador de proceso.
+    var alertStringsERROR = { text:"Ocurrió un error, intentelo de nuevo. Si el problema persiste contacte con el administrador", title:"ERROR", confirmButtonLabel:"Aceptar"};
+    var alertOptionsERROR = { height: 200, width: 450  };
     Xrm.Utility.showProgressIndicator("Please wait...");
-    console.log("Flow Call request sent");
-};
+    //Haremos la llamada a nuestro Power Automate.
+    try {
+        await fetch("PowerAutomateURL",
+        {
+            "method":"POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        console.log("Flow call finished Ok");
+        Xrm.Utility.closeProgressIndicator();
+        Xrm.Navigation.openAlertDialog(alertStrings, alertOptions);
+    
+    } catch (error) {
+    
+    Xrm.Utility.closeProgressIndicator();
+    console.log(`
+    Flow Call finished with error. Status code: ${error}
+    `)
+    Xrm.Navigation.openAlertDialog(alertStringsERROR, alertOptionsERROR);
+    }
+    }
